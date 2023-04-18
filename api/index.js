199067bea546b6c8c8140b60223d9952ae161ed2
salman <salmann9977@gmail.com>
2023-04-18 23:50:56 +0530

    dev branch setup and server setup

--- api/index.js
+++ api/index.js
@@ -1,2 +1,33 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("connected to mongodb");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:3000", credentials: true}));
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong";
+    return res.status(errorStatus).send(errorMessage);
+});
+
+app.listen(8800, async () => {
+    await connectDb();
+    console.log("server running on port 8800")
+});
