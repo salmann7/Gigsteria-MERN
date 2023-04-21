2053fe61694be441fd3fc21db3f64a181e8ef289
salman <salmann9977@gmail.com>
2023-04-21 18:14:33 +0530

    hook for register modal

---
+++ client/src/components/modals/RegisterModal.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
+import React, { useCallback, useState } from 'react'
+import { toast } from "react-hot-toast";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+
+
+const RegisterModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RegisterModal
