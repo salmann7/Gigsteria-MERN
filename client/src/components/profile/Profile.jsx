7a2837810cc16988f0efb85a0acd6ccc01849cb6
salman <salmann9977@gmail.com>
2023-05-03 08:30:33 +0530

    fix for profile ui responsiveness and layout

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -87,7 +87,7 @@ const Profile = () => {
   return (
     <Container>
         <div className="pb-24 flex flex-col md:flex-row gap-3">
-            <div className="md:w-1/4">
+            <div className="md:w-1/3 lg:w-1/4">
                 <div className="flex flex-col gap-3">
                     <div className="bg-white shadow-lg flex flex-col gap-3 items-center justify-center p-4">
                         <img src={user?.picture || '/images/placeholder.jpg'} alt="profile pic" className=' w-44 h-44 rounded-full' />
@@ -101,7 +101,30 @@ const Profile = () => {
                         </div>
                         <button className='text-center px-5 py-2 rounded-full bg-green-500 border-none hover:bg-green-600 hover:shadow-sm transition text-white text-xl font-semibold'>Follow</button>
                     </div>
-                    <div className="bg-neutral-50 shadow-md">
+                    <div className="bg-white shadow-lg p-4 flex flex-col gap-4">
+                        <h3 className='font-semibold text-lg text-neutral-800'>Achievements</h3>
+                        <div className="flex flex-row justify-between text-neutral-500 font-semibold text-sm">
+                          <h6>Total Rating:- </h6>
+                          <div className="flex flex-row">{getRatingStars(3)}</div>
+                        </div>
+                        <div className="flex flex-row justify-between text-neutral-500 font-semibold text-sm">
+                          <h6>Total Orders:- </h6>
+                          <p>55</p>
+                        </div>
+                    </div>
+                    <div className="bg-white shadow-md">
+                        <div className="flex flex-col gap-4 p-4">
+                            <h3 className='font-semibold text-lg text-neutral-800'>Testimonials</h3>
+                            <div className="flex flex-nowrap overflow-x-auto">
+                          {comments ? (comments.map((comment) => (
+                            <div key={comment.id} className="flex-shrink-0 w-72 mr-4">
+                                <ProfileComments data={comment} />
+                            </div>
+                          ))):(null)}
+                        </div>
+                        </div>
+                    </div>
+                    <div className="bg-neutral-50 shadow-md hidden md:block">
                         <div className="flex flex-col gap-3 p-4">
                             <div className="flex flex-row justify-between">
                               <h3 className='font-semibold text-neutral-800 text-xl'>Skills</h3>
@@ -114,9 +137,19 @@ const Profile = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="bg-neutral-50 shadow-md hidden md:block">
+                        <div className="flex flex-col gap-4 p-4">
+                          <h3 className='font-semibold text-lg text-neutral-800'>Contacts</h3>
+                          <div className="flex flex-row gap-4 text-gray-700 justify-around">
+                            <MdEmail size={28} className='hover:cursor-pointer'/>
+                            <BsTwitter size={28} className='hover:cursor-pointer' />
+                            <TiSocialLinkedin size={28} className='hover:cursor-pointer'/>
+                          </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="md:w-2/4 ">
+            <div className="md:w-2/3 lg:w-3/4 ">
                 <div className="flex flex-col gap-6 shadow-sm bg-white">
                     <div className="shadow-sm bg-white flex flex-col gap-3 p-4">
                         <h3 className='font-semibold text-lg text-neutral-800 text-center'>Community</h3>
@@ -148,7 +181,30 @@ const Profile = () => {
                     </div>
                 </div>
             </div>
-            <div className="md:w-1/4">
+            <div className="bg-neutral-50 shadow-md block md:hidden">
+                        <div className="flex flex-col gap-3 p-4">
+                            <div className="flex flex-row justify-between">
+                              <h3 className='font-semibold text-neutral-800 text-xl'>Skills</h3>
+                              <button className='text-white text-xs px-4 py-2 bg-blue-400 rounded-full border-none hover:bg-blue-500 hover:cursor-pointer hover:shadow-sm'>+ Add</button>
+                            </div>
+                            <div className="flex flex-row gap-3 flex-wrap">
+                              {skills.map((skill) => (
+                                  <div key={skill.id} className="hover:cursor-pointer bg-green-50 text-green-500 text-sm px-4 py-2">{skill.name}</div>
+                              ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-neutral-50 shadow-md block md:hidden">
+                        <div className="flex flex-col gap-4 p-4">
+                          <h3 className='font-semibold text-lg text-neutral-800'>Contacts</h3>
+                          <div className="flex flex-row gap-4 text-gray-700 justify-around">
+                            <MdEmail size={28} className='hover:cursor-pointer'/>
+                            <BsTwitter size={28} className='hover:cursor-pointer' />
+                            <TiSocialLinkedin size={28} className='hover:cursor-pointer'/>
+                          </div>
+                        </div>
+                    </div>
+            {/* <div className="md:w-1/4">
                 <div className="flex flex-col gap-3">
                     <div className="bg-white shadow-lg p-4 flex flex-col gap-4">
                         <h3 className='font-semibold text-lg text-neutral-800'>Achievements</h3>
@@ -184,7 +240,7 @@ const Profile = () => {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
         </div>
     </Container>
   )
