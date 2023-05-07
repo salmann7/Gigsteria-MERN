ede2dff04bf0ef2b4d24e3d0c4abdd271ef02f3c
salman <salmann9977@gmail.com>
2023-05-08 04:36:25 +0530

    fix for domain:'.onrender.com' main domain

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 const accessTokenCookieOptions = {
     maxAge: 900000, // 15 mins
     httpOnly: false,
-    domain: "localhost",
+    domain: ".onrender.com",
     path: "/",
     sameSite: "lax",
     secure: true,
